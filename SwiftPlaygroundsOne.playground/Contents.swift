01477e6dfeddf24d35b2d3bf12148e3354087f37
CM Student <cmstudent@UHWO-ACM-22.local>
2019-01-29 12:40:19 -1000

    Completed Assignment #2 Video
    Intro to True or False or Boolean Variables

--- SwiftPlaygroundsOne.playground/Contents.swift
+++ SwiftPlaygroundsOne.playground/Contents.swift
@@ -14,3 +14,17 @@ var mySecondVariable = "and look I added 2 strings together"
 //Lets add two strings together
 var myThirdVariable = myFirstVariable + mySecondVariable
 print(myThirdVariable)
+
+// Assignment #2 Introduction to True and False
+var appState = false
+
+print(appState)
+
+// Cpmparing variables using > (greater than) or (less than)
+var numberOne = 5
+var numberTwo = 7
+//Create a variable that holds the comparison on the two variables we just created
+
+var compare = numberOne > numberTwo
+
+
